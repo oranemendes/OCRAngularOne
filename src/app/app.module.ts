515dc642801113cecacd470addc1a0cb6631a808
oranemendes <orane.mendes@gmail.com>
2019-06-19 09:45:46 +0200

    adding node modules to git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { PostListItemComponent } from './post-list/post-list/post-list-item/post-list-item.component';
+import { PostListItemComponent } from './post-list/post-list-item/post-list-item.component';
 import { PostItemComponent } from './post-list/post-item/post-item.component';
 
 @NgModule({
--- src/app/post-list/post-list-item/post-list-item.component.ts
+++ src/app/post-list/post-list-item/post-list-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Post} from '../../post.model';
+import {Post} from '../post.model';
 
 @Component({
   selector: 'app-post-list-item',
